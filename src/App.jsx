f189c5b9ae953a9a5bb14149b931cff63d19b27d
psolis1997 <patricia-anne@outlook.com>
2023-06-28 14:23:42 +1200

    Portfolio Website

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="home">
+        <img src="src/assets/logo.png" alt="" />
+        <div className='text'>
+          <p>
+            Patricia's <span className='info'>about, works, and contact</span> will be live on
+            <span className='date'>&nbsp; Monday 3 July, 2023.</span>
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
